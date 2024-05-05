39e39a9c9ed8d5d25c0047e3aa644778c178c9c0
jaga269 <126978681+jaga269@users.noreply.github.com>
2024-05-05 19:57:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.getElementById('openBtn1').addEventListener('click', function() {
+document.getElementById('popupBox1').style.display = 'block';
+document.getElementById('myphoto').style.display = 'block';
+document.getElementById('myphoto').style.opacity = '1';
+document.getElementById('blurBackground').style.display = 'block';
+});
+
+document.querySelectorAll('.close-btn').forEach(function(button) {
+button.addEventListener('click', function() {
+this.parentNode.style.display = 'none';
+document.getElementById('blurBackground').style.display = 'none';
+});
+});
